cda50229b9a4002e8028821d7c703cfd70761b62
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2022-01-02 07:22:54 -0800

    added mock table

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 import streamlit as sl
+import pandas as pd
 
 url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
 
@@ -27,3 +28,31 @@ except (ConnectionError, Timeout, TooManyRedirects) as e:
   print(e)
 sl.title("Crypto Data Science App")
 sl.image("crypto-app-logo.jpeg")
+
+# creating table
+# create array for each column
+name = [1, 2, 3, 4, 5, 6] 
+symbol = [1, 2, 3, 4, 5, 6]
+price = [1, 2, 3, 4, 5, 6]
+volume_24h = [1, 2, 3, 4, 5, 6]
+volume_change_24h = [1, 2, 3, 4, 5, 6]
+percent_change_1h = [1, 2, 3, 4, 5, 6]
+percent_change_24h = [1, 2, 3, 4, 5, 6]
+percent_change_7d = [1, 2, 3, 4, 5, 6]
+market_cap = [1, 2, 3, 4, 5, 6]
+
+def show_table():
+    crypto_data_table = pd.DataFrame(columns=['name', 'symbol', 'price', 'volume_24h', 'volume_change_24h', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d', 'market_cap'])
+    crypto_data_table['name'] = name
+    crypto_data_table['symbol'] = symbol
+    crypto_data_table['price'] = price
+    crypto_data_table['volume_24h'] = volume_24h
+    crypto_data_table['volume_change_24h'] = volume_change_24h
+    crypto_data_table['percent_change_1h'] = percent_change_1h
+    crypto_data_table['percent_change_24h'] = percent_change_24h
+    crypto_data_table['percent_change_7d'] = percent_change_7d
+    crypto_data_table['market_cap'] = market_cap
+    sl.table(crypto_data_table)
+
+show_table()
+
