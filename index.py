d85a26818d12fadccf29170adf09816e9ba1f370
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2022-01-04 17:04:12 -0800

    made table full of crypto data

--- index.py
+++ index.py
@@ -30,8 +30,38 @@ except (ConnectionError, Timeout, TooManyRedirects) as e:
 
 data = data["data"]
 
+name_array = [] 
+symbol_array = []
+price_array = []
+volume_24h_array = []
+volume_change_24h_array = []
+percent_change_1h_array = []
+percent_change_24h_array = []
+percent_change_7d_array = []
+market_cap_array = []
+
 for point in data:
-    print("yeahhhh buddy")
+    name = point["name"]
+    symbol = point["symbol"]
+    price = point["quote"]["USD"]["price"]
+    print(price)
+    volume_24h = point["quote"]["USD"]["volume_24h"]
+    volume_change_24h = point["quote"]["USD"]["volume_change_24h"]
+    percent_change_1h = point["quote"]["USD"]["percent_change_1h"]
+    percent_change_24h = point["quote"]["USD"]["percent_change_24h"]
+    percent_change_7d = point["quote"]["USD"]["percent_change_7d"]
+    market_cap = point["quote"]["USD"]["market_cap"]
+
+    name_array.append(name)
+    symbol_array.append(symbol)
+    price_array.append("{:.2f}".format(price))
+    volume_24h_array.append(volume_24h)
+    volume_change_24h_array.append(volume_change_24h)
+    percent_change_1h_array.append(percent_change_1h)
+    percent_change_24h_array.append(percent_change_24h)
+    percent_change_7d_array.append(percent_change_7d)
+    market_cap_array.append(market_cap)
+
 
 sl.title("Crypto Data Science App")
 
@@ -39,28 +69,19 @@ sl.image("crypto-app-logo.jpeg")
 
 # creating table
 # create array for each column
-name = [1, 2, 3, 4, 5, 6] 
-symbol = [1, 2, 3, 4, 5, 6]
-price = [1, 2, 3, 4, 5, 6]
-volume_24h = [1, 2, 3, 4, 5, 6]
-volume_change_24h = [1, 2, 3, 4, 5, 6]
-percent_change_1h = [1, 2, 3, 4, 5, 6]
-percent_change_24h = [1, 2, 3, 4, 5, 6]
-percent_change_7d = [1, 2, 3, 4, 5, 6]
-market_cap = [1, 2, 3, 4, 5, 6]
+
 
 def show_table():
     crypto_data_table = pd.DataFrame(columns=['name', 'symbol', 'price', 'volume_24h', 'volume_change_24h', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d', 'market_cap'])
-    crypto_data_table['name'] = name
-    crypto_data_table['symbol'] = symbol
-    crypto_data_table['price'] = price
-    crypto_data_table['volume_24h'] = volume_24h
-    crypto_data_table['volume_change_24h'] = volume_change_24h
-    crypto_data_table['percent_change_1h'] = percent_change_1h
-    crypto_data_table['percent_change_24h'] = percent_change_24h
-    crypto_data_table['percent_change_7d'] = percent_change_7d
-    crypto_data_table['market_cap'] = market_cap
+    crypto_data_table['name'] = name_array
+    crypto_data_table['symbol'] = symbol_array
+    crypto_data_table['price'] = price_array
+    crypto_data_table['volume_24h'] = volume_24h_array
+    crypto_data_table['volume_change_24h'] = volume_change_24h_array
+    crypto_data_table['percent_change_1h'] = percent_change_1h_array
+    crypto_data_table['percent_change_24h'] = percent_change_24h_array
+    crypto_data_table['percent_change_7d'] = percent_change_7d_array
+    crypto_data_table['market_cap'] = market_cap_array
     sl.table(crypto_data_table)
 
 show_table()
-
