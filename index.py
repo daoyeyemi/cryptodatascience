e875d1b9f626900fb4c175f5ba04f08d0e7edd38
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2022-01-06 11:47:28 -0800

    madedata cutoff and introduced sidebar to application

--- index.py
+++ index.py
@@ -29,6 +29,7 @@ except (ConnectionError, Timeout, TooManyRedirects) as e:
   print(e)
 
 data = data["data"]
+data = data[0:25]
 
 name_array = [] 
 symbol_array = []
@@ -40,28 +41,30 @@ percent_change_24h_array = []
 percent_change_7d_array = []
 market_cap_array = []
 
-for point in data:
-    name = point["name"]
-    symbol = point["symbol"]
-    price = point["quote"]["USD"]["price"]
-    print(price)
-    volume_24h = point["quote"]["USD"]["volume_24h"]
-    volume_change_24h = point["quote"]["USD"]["volume_change_24h"]
-    percent_change_1h = point["quote"]["USD"]["percent_change_1h"]
-    percent_change_24h = point["quote"]["USD"]["percent_change_24h"]
-    percent_change_7d = point["quote"]["USD"]["percent_change_7d"]
-    market_cap = point["quote"]["USD"]["market_cap"]
+def add_data_to_table():
+    for point in data:
+        name = point["name"]
+        symbol = point["symbol"]
+        price = point["quote"]["USD"]["price"]
+        print(price)
+        volume_24h = point["quote"]["USD"]["volume_24h"]
+        volume_change_24h = point["quote"]["USD"]["volume_change_24h"]
+        percent_change_1h = point["quote"]["USD"]["percent_change_1h"]
+        percent_change_24h = point["quote"]["USD"]["percent_change_24h"]
+        percent_change_7d = point["quote"]["USD"]["percent_change_7d"]
+        market_cap = point["quote"]["USD"]["market_cap"]
 
-    name_array.append(name)
-    symbol_array.append(symbol)
-    price_array.append("{:.2f}".format(price))
-    volume_24h_array.append(volume_24h)
-    volume_change_24h_array.append(volume_change_24h)
-    percent_change_1h_array.append(percent_change_1h)
-    percent_change_24h_array.append(percent_change_24h)
-    percent_change_7d_array.append(percent_change_7d)
-    market_cap_array.append(market_cap)
+        name_array.append(name)
+        symbol_array.append(symbol)
+        price_array.append("{:,.2f}".format(price))
+        volume_24h_array.append("{:,.2f}".format(volume_24h))
+        volume_change_24h_array.append("{:,.2f}".format(volume_change_24h))
+        percent_change_1h_array.append("{:,.2f}".format(percent_change_1h))
+        percent_change_24h_array.append("{:,.2f}".format(percent_change_24h))
+        percent_change_7d_array.append("{:,.2f}".format(percent_change_7d))
+        market_cap_array.append("{:,.2f}".format(market_cap))
 
+add_data_to_table()
 
 sl.title("Crypto Data Science App")
 
@@ -69,7 +72,7 @@ sl.image("crypto-app-logo.jpeg")
 
 # creating table
 # create array for each column
-
+sl.write("Top 25 Cryptocurrencies")
 
 def show_table():
     crypto_data_table = pd.DataFrame(columns=['name', 'symbol', 'price', 'volume_24h', 'volume_change_24h', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d', 'market_cap'])
@@ -84,4 +87,6 @@ def show_table():
     crypto_data_table['market_cap'] = market_cap_array
     sl.table(crypto_data_table)
 
-show_table()
+show_table() 
+
+sl.sidebar.multiselect('Cryptocurrency', symbol_array, symbol_array)
