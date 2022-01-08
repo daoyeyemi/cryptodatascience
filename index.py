ee8812f81ed67ce02a544c76829826dcd5d6a1f5
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2022-01-08 07:59:27 -0800

    synced multiselect in sidebar to main dataframe

--- index.py
+++ index.py
@@ -5,6 +5,13 @@ import streamlit as sl
 import pandas as pd
 import matplotlib.pyplot as plt
 
+## To List
+# - hide api key
+# - clean comments
+# - color code graph
+# - 'objectify' code 
+# change layout of app
+
 url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
 
 parameters = {
@@ -31,23 +38,11 @@ except (ConnectionError, Timeout, TooManyRedirects) as e:
 
 yooo = crypto_data['data']
 
-symbols = []
-
 sl.sidebar.header('Input Options')
 
 coin_number = sl.sidebar.slider("Number of the top cryptocurrencies you're searching for:", 1, 50, 50)
 
 yooo = yooo[0:coin_number]
-
-for point in yooo:    
-    symbol = point["symbol"]
-    symbols.append(symbol)
-
-symbols = symbols[0:coin_number]
-
-sl.sidebar.multiselect('Cryptocurrency', symbols, symbols)
-
-time_frame_percent = sl.sidebar.selectbox('Time frame for percent change', ['7d', '24h', '1h'])
 
 name_array = [] 
 symbol_array = []
@@ -59,7 +54,7 @@ percent_change_24h_array = []
 percent_change_7d_array = []
 market_cap_array = []
 
-# def add_data_to_table():
+
 for point in yooo:
     name = point["name"]
     symbol = point["symbol"]
@@ -77,34 +72,16 @@ for point in yooo:
     price_array.append("{:,.2f}".format(price))
     volume_24h_array.append("{:,.2f}".format(volume_24h))
     volume_change_24h_array.append("{:,.2f}".format(volume_change_24h))
-    # percent_change_1h_array.append("{:,.2f}".format(percent_change_1h))
-    # percent_change_24h_array.append("{:,.2f}".format(percent_change_24h))
-    # percent_change_7d_array.append("{:,.2f}".format(percent_change_7d))
     percent_change_1h_array.append(percent_change_1h)
     percent_change_24h_array.append(percent_change_24h)
     percent_change_7d_array.append(percent_change_7d)
     market_cap_array.append("{:,.2f}".format(market_cap))
-        
-    # percent_change_1h_array = list(map(int, percent_change_1h_array))
-    # percent_change_24h_array = list(map(int, percent_change_24h_array))
-    # percent_change_7d_array = list(map(int, percent_change_7d_array))
-
-# add_data_to_table()
 
 sl.title("Crypto Data Science App")
 
 sl.image("crypto-app-logo.jpeg")
 
-# creating table
-# create array for each column
 sl.write("Top 25 Cryptocurrencies")
-
-# def show_tables():
-# percent_change_1h_array = list(map(float, percent_change_1h_array))
-# percent_change_1h_array = list(map(int, percent_change_1h_array))
-print(percent_change_1h_array)
-# percent_change_24h_array = list(map(float, percent_change_24h_array))
-# percent_change_7d_array = list(map(float, percent_change_7d_array))
 
 crypto_data_table = pd.DataFrame(columns=['name', 'symbol', 'price', 'volume_24h', 'volume_change_24h', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d', 'market_cap'])
 crypto_data_table['name'] = name_array
@@ -113,13 +90,25 @@ crypto_data_table['price'] = price_array
 crypto_data_table['volume_24h'] = volume_24h_array
 crypto_data_table['volume_change_24h'] = volume_change_24h_array
 crypto_data_table['percent_change_1h'] = percent_change_1h_array
-print(percent_change_1h_array)
 crypto_data_table['percent_change_24h'] = percent_change_24h_array
 crypto_data_table['percent_change_7d'] = percent_change_7d_array
 crypto_data_table['market_cap'] = market_cap_array
-sl.table(crypto_data_table)
+# sl.table(crypto_data_table)
 
-# show_tables() 
+all_coins = crypto_data_table['symbol']
+
+crypto_coins_selected = sl.sidebar.multiselect('Cryptocurrency', all_coins, all_coins)
+# all cryptocurrencies that are included in multiselect will be included here
+# filtering data based on
+# if crypto_coins_selected are in the 'symbol' part of dataframe then they are now part of the 'crypto_dataframe'
+crypto_coins = crypto_data_table[ (crypto_data_table['symbol'].isin(crypto_coins_selected)) ]
+
+crypto_dataframe = crypto_coins[0:coin_number]
+
+sl.table(crypto_dataframe)
+
+time_frame_percent = sl.sidebar.selectbox('Time frame for percent change', ['7d', '24h', '1h'])
+
 sl.subheader('Bar plot of percent changes in crypto price')
 subheader_percent_changes = 0;
 positive_percent_change_dataframe = pd.DataFrame(columns=['name', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d'])
@@ -136,27 +125,9 @@ else:
     percent_change = percent_change_1h_array
     subheader_percent_changes = sl.write('*percent changes within an hour period*')
 
-# percent_change = percent_change_7d_array
-# positive_percent_change_dataframe['percent_change_1h'] = percent_change_1h_array >= 0 
-# positive_percent_change_dataframe['percent_change_24h'] = percent_change_24h_array >= 0
-# positive_percent_change_dataframe['percent_change_7d'] = percent_change_7d_array >= 0
 ax.bar(crypto_symbols, percent_change, color='blue')
 plt.xticks(rotation=70)
 plt.xlabel("Cryptocurrency")
 plt.ylabel("Percentage change within specified time period")
 fig.canvas.draw()
 sl.pyplot(plt)
-
-# sl.subheader('Negative percent change in crypto price')
-# negative_percent_change_dataframe = pd.DataFrame(columns=['name', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d'])
-# fig = plt.figure()
-# ax = fig.add_axes([0, 0, 2, 2])
-# crypto_symbols = symbol_array
-# percent_change = percent_change_7d_array
-# # negative_percent_change_dataframe['percent_change_1h'] = percent_change_1h_array < 0 
-# # negative_percent_change_dataframe['percent_change_24h'] = percent_change_24h_array < 0 
-# # negative_percent_change_dataframe['percent_change_7d'] = percent_change_7d_array < 0 
-# ax.bar(crypto_symbols, percent_change, color='red')
-# plt.xticks(rotation=70)
-# fig.canvas.draw()
-# sl.pyplot(plt)
